dfa334775c180fd2ded21d07a0b3b46a6c07d51c
gabriellevy <mdeharbe@hotmail.com>
2025-06-26 21:36:54 +0200

    toutes vices et vertus

--- src/types/ViceVertu.ts
+++ src/types/ViceVertu.ts
@@ -10,23 +10,55 @@ export type ViceVertu = {
 export enum TypeVice {
     cupide = "Cupide",
     colerique = "Colérique",
+    cruel = "Cruel",
+    envieux = "Envieux",
+    gourmand = "Gourmand",
+    lache = "Lâche",
+    luxurieux = "Luxurieux",
+    orgueilleux = "Orgueilleux",
+    paresseux = "Paresseux",
+    trompeur = "Trompeur",
 }
 
 export enum TypeVertu {
     genereux = "Généreux",
     prudent = "Prudent",
+    clement = "Clément",
+    bienveillant = "Bienveillant",
+    sobre = "Sobre",
+    valeureux = "Valeureux",
+    chaste = "Chaste",
+    humble = "Humble",
+    travailleur = "Travailleur",
+    loyal = "Loyal",
 }
 
 export function getViceOppose(typeVertu: TypeVertu): TypeVice {
     switch (typeVertu) {
         case TypeVertu.genereux : return TypeVice.cupide;
         case TypeVertu.prudent : return TypeVice.colerique;
+        case TypeVertu.clement : return TypeVice.cruel;
+        case TypeVertu.bienveillant : return TypeVice.envieux;
+        case TypeVertu.sobre : return TypeVice.gourmand;
+        case TypeVertu.valeureux : return TypeVice.lache;
+        case TypeVertu.chaste : return TypeVice.luxurieux;
+        case TypeVertu.humble : return TypeVice.orgueilleux;
+        case TypeVertu.travailleur : return TypeVice.paresseux;
+        case TypeVertu.loyal : return TypeVice.trompeur;
     }
 }
 export function getVertuOppose(typeVice: TypeVice): TypeVertu {
     switch (typeVice) {
         case TypeVice.cupide : return TypeVertu.genereux;
         case TypeVice.colerique : return TypeVertu.prudent;
+        case TypeVice.cruel : return TypeVertu.clement;
+        case TypeVice.envieux : return TypeVertu.bienveillant;
+        case TypeVice.gourmand : return TypeVertu.sobre;
+        case TypeVice.lache : return TypeVertu.valeureux;
+        case TypeVice.luxurieux : return TypeVertu.chaste;
+        case TypeVice.orgueilleux : return TypeVertu.humble;
+        case TypeVice.paresseux : return TypeVertu.travailleur;
+        case TypeVice.trompeur : return TypeVertu.loyal;
     }
 }
 
@@ -52,10 +84,8 @@ export function viceVertuDeBase(): Array<ViceVertu> {
 
 export function valeurViceVertuAleatoire(): number {
     const rand:number = Math.random();
-    console.log(rand);
-    console.log("(rand < 0.8 : " + (rand < 0.8));
+
     if (rand < 0.8) return 0;
-    console.log("(rand < 0.9 : " + (rand < 0.9));
 
     if (rand < 0.9) {
         // positif
