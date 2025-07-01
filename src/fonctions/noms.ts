a8c2703aa972c24361957ec68b114678ee825a82
gabriellevy <mdeharbe@hotmail.com>
2025-07-01 22:02:37 +0200

    noms celtes

--- src/fonctions/noms.ts
+++ src/fonctions/noms.ts
@@ -8,12 +8,14 @@ import {
 } from "../donnees/coteries/romains/noms_romains";
 import {getRandomInt} from "./random";
 import {Sexe} from "../types/Perso";
+import {CELTES_PRENOMS_F, CELTES_PRENOMS_M} from "../donnees/coteries/celtes/noms_celtes";
 
 export function getNom(coterie: Coterie, sexe: Sexe): string {
     switch (coterie) {
         case Coterie.romains : return sexe === Sexe.male ?
             ROMAINS_NOMS_M1[getRandomInt(ROMAINS_NOMS_M1.length)] :
             ROMAINS_NOMS_F[getRandomInt(ROMAINS_NOMS_F.length)];
+        case Coterie.celtes: return ""; // les celtes n'ont pas de noms de familles
     }
     return "pas de noms pour cette coterie : " + coterie;
 }
@@ -23,6 +25,9 @@ export function getPrenom(coterie: Coterie, sexe: Sexe): string {
         case Coterie.romains : return sexe === Sexe.male ?
             ROMAINS_PRENOMS_M[getRandomInt(ROMAINS_PRENOMS_M.length)] :
             ROMAINS_PRENOMS_F[getRandomInt(ROMAINS_PRENOMS_F.length)];
+        case Coterie.celtes : return sexe === Sexe.male ?
+            CELTES_PRENOMS_M[getRandomInt(CELTES_PRENOMS_M.length)] :
+            CELTES_PRENOMS_F[getRandomInt(CELTES_PRENOMS_F.length)];
     }
     return "pas de prénoms pour cette coterie : " + coterie;
 }
@@ -42,6 +47,7 @@ export function getPatronyme(coterie: Coterie, sexe: Sexe): string {
                                     + " " + getNom(coterie, sexe)
                                         // cognomen romain
                                     + " " +  getCognomen(coterie, sexe);
+        case Coterie.celtes : return getPrenom(coterie, sexe);
     }
     return "pas de patronyme pour cette coterie : " + coterie;
 }
