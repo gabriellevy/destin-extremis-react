51903e554e159b8bfb0668f8caea62646af9d36f
gabriellevy <mdeharbe@hotmail.com>
2025-07-06 16:54:21 +0200

    noms français

--- src/fonctions/noms.ts
+++ src/fonctions/noms.ts
@@ -10,6 +10,7 @@ import {getRandomInt} from "./random";
 import {Sexe} from "../types/Perso";
 import {CELTES_PRENOMS_F, CELTES_PRENOMS_M} from "../donnees/coteries/celtes/noms_celtes";
 import {PARTIE_NOM_SKAVEN1, PARTIE_NOM_SKAVEN2, PARTIE_NOM_SKAVEN3} from "../donnees/coteries/skavens/noms_skavens";
+import {NOM, PRENOM_F, PRENOM_M} from "../donnees/coteries/aucune/noms_francais";
 
 export function getNom(coterie: Coterie, sexe: Sexe): string {
     switch (coterie) {
@@ -19,6 +20,7 @@ export function getNom(coterie: Coterie, sexe: Sexe): string {
         case Coterie.celtes:
         case Coterie.skavens:
             return ""; // les celtes n'ont pas de noms de familles
+        case Coterie.aucune : return NOM[getRandomInt(NOM.length)];
     }
     return "pas de noms pour cette coterie : " + coterie;
 }
@@ -34,6 +36,9 @@ export function getPrenom(coterie: Coterie, sexe: Sexe): string {
         case Coterie.skavens: return PARTIE_NOM_SKAVEN1[getRandomInt(PARTIE_NOM_SKAVEN1.length)]
             + PARTIE_NOM_SKAVEN2[getRandomInt(PARTIE_NOM_SKAVEN3.length)]
             + PARTIE_NOM_SKAVEN3[getRandomInt(PARTIE_NOM_SKAVEN3.length)];
+        case Coterie.aucune: return sexe === Sexe.male ?
+            PRENOM_M[getRandomInt(PRENOM_M.length)] :
+            PRENOM_F[getRandomInt(PRENOM_F.length)];
     }
     return "pas de prénoms pour cette coterie : " + coterie;
 }
@@ -53,7 +58,11 @@ export function getPatronyme(coterie: Coterie, sexe: Sexe): string {
                                     + " " + getNom(coterie, sexe)
                                         // cognomen romain
                                     + " " +  getCognomen(coterie, sexe);
-        case Coterie.celtes : return getPrenom(coterie, sexe);
+        case Coterie.celtes :
+        case Coterie.skavens :
+            return getPrenom(coterie, sexe);
+        case Coterie.aucune : return getPrenom(coterie, sexe)
+            + " " + getNom(coterie, sexe)
     }
     return "pas de patronyme pour cette coterie : " + coterie;
 }
