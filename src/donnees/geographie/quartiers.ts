9b76bd8dd1ff0e47dfb274dc873d677941734a51
gabriellevy <mdeharbe@hotmail.com>
2025-07-30 23:26:20 +0200

    noisiel

--- src/donnees/geographie/quartiers.ts
+++ src/donnees/geographie/quartiers.ts
@@ -37,6 +37,11 @@ export function descriptionQuartier(quartier: Quartier): string {
                 + "Cette crête a une altitude équivalente à celle des alpes mais est encore plus abrupte. <br/>"
                 + "Elle est naturellement très difficile à aménager mais les schweizer l’ont trouvée à leur goût et l’ont transformée de manière si ingénieuse qu’elle est devenue prospère et très bien desservie par les chemins de fers, surtout dans la partie proche de la Ville. ";
             break;
+            case Quartier.noisiel:
+            texte += "Noisiel est un petit quartier réquisitionné par les [Achéron](https://www.notion.so/Ach-ron-acdb44a8711640f5a6d9527e5e656d5a?pvs=21) pour habiter le proche centre de la Ville et ainsi remplir les obligations légales pour être une coterie siégeant à l’assemblée. <br/>"
+                + "Malheureusement leur influence a perverti le quartier et en a fait un lieu désolé et morbide où très peu d’Extrémiens viennent s’installer. <br/>"
+                + "Ils voulaient augmenter leur influence politique mais l’exemple de leur gouvernance et de leur influence magique a au contraire fait d’eux un exemple à ne pas suivre pour tous ceux qui ont visité Noisiel. <br/>";
+            break;
         default:
             texte += "Il manque la description de cet endroit : " + quartier;
     }
