092474add84535ce9a53f89c285aba5c8e667a71
gabriellevy <mdeharbe@hotmail.com>
2025-08-06 20:33:36 +0200

    corrige sliders

--- src/ChoixDeCoterie/ChoixDeCoterie.tsx
+++ src/ChoixDeCoterie/ChoixDeCoterie.tsx
@@ -17,8 +17,8 @@ const ChoixDeCoterie : React.FC = () => {
         defaultValues: defaultValues
     });
 
-    const idsSliders: TypeMauvais[] = Object.values(TypeMauvais);
-    const sliders = watch(Object.keys(TypeMauvais) as Array<TypeMauvais>);
+    const idsSliders: string[] = Object.values(TypeMauvais);
+    const sliders = watch(Object.values(TypeMauvais) as Array<TypeMauvais>);
 
     const total = sliders.reduce((sum, value) => sum + Math.abs(value), 0);
 
@@ -29,34 +29,35 @@ const ChoixDeCoterie : React.FC = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            {idsSliders.map((idSlider: TypeMauvais, indexSliderAffiche: number) => (
-                <Grid2 container spacing={0} sx={{ mb: 2 }} columns={8}>
-                    <Grid2 size={4}>
-                        {idSlider} :
-                    </Grid2>
-                    <Grid2 size={4}>
-                        <input
-                            type="range"
-                            min="-3"
-                            max="3"
-                            {...register(idSlider as keyof FormData, {
-                                validate: (value: number) => {
-                                    const newTotal = sliders
-                                        .map((v, index) => index === indexSliderAffiche ? value : v)
-                                        .reduce((sum, v) => sum + Math.abs(v), 0);
-                                    return newTotal <= 5 || 'Total des déplacements ne peut pas dépasser 5 crans';
-                                }
-                            })}
-                        />
-                    {errors[idSlider as TypeMauvais] && <p>{errors[idSlider as TypeMauvais]?.message}</p>}
-                    </Grid2>
-                </Grid2>
-            ))}
+            <Grid2 container spacing={0} sx={{ mb: 2 }} columns={12}>
+                {idsSliders.map((idSlider: string, indexSliderAffiche: number) => (
+                    <>
+                        <Grid2 size={3}>
+                            {idSlider} :
+                        </Grid2>
+                        <Grid2 size={3}>
+                            <input
+                                type="range"
+                                min="-3"
+                                max="3"
+                                {...register(idSlider as keyof FormData, {
+                                    validate: (value: number) => {
+                                        const newTotal = sliders
+                                            .map((v, index) => index === indexSliderAffiche ? value : v)
+                                            .reduce((sum, v) => sum + Math.abs(v), 0);
+                                        return newTotal <= 5 || 'Total des déplacements ne peut pas dépasser 5 crans';
+                                    }
+                                })}
+                            />
+                        {errors[idSlider as TypeMauvais] && <p>{errors[idSlider as TypeMauvais]?.message}</p>}
+                        </Grid2>
+                    </>
+                ))}
+            </Grid2>
             <p>Total des déplacements: {total}</p>
             <button type="submit" disabled={total > 5}>Valider</button>
         </form>
     );
 };
-
 
 export default ChoixDeCoterie
