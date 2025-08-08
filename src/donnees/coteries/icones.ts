83e533726914d7100a6c29d2a8455a26d41e207d
gabriellevy <mdeharbe@hotmail.com>
2025-08-09 00:33:24 +0200

    image Démokratos

--- src/donnees/coteries/icones.ts
+++ src/donnees/coteries/icones.ts
@@ -1,7 +1,7 @@
 import {Coterie} from "../../types/Coterie";
 
 export const iconesCot: Record<Coterie, string> = {
-    [Coterie.culte_du_plaisir]: "",
+    [Coterie.culte_du_plaisir]: "https://raw.githubusercontent.com/gabriellevy/destin-extremis-react/refs/heads/main/images/coteries/icones/Culte%20du%20plaisir.png",
     "Esprit de la nature": "",
     Aucune: "",
     [Coterie.bastets]: "https://raw.githubusercontent.com/gabriellevy/destin-extremis-react/refs/heads/main/images/coteries/icones/Bastet.png",
