987baa525883d8aaa1a5f28f3991d38c7a9a6f0c
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2025-09-12 21:30:07 +0200

    nettoyage calendrier

--- src/donnees/evts/evts_calendrier.ts
+++ src/donnees/evts/evts_calendrier.ts
@@ -7,26 +7,14 @@ export const evts_calendrier: GroupeEvts = {
             {
                 id: "calendrier1",
                 description: async (_perso: Perso): Promise<string> => {
-                     return "Aujourd'hui est le jour du nouvel an, le terrible jour des sorcières ! " +
-                    "La ligne qui sépare le monde des vivants de celui des morts est plus ténue cette nuit ci et les esprit de ceux qui nous ont quitté font parfois leur retour. " +
-                    "Vous et toute votre famille restez cloîtrés chez vous."},
+                     return "Aujourd'hui est le jour du nouvel an."},
                 conditions: (perso:Perso):boolean => perso.mois === enumMois.vendemiaire,
                 image: "https://raw.githubusercontent.com/gabriellevy/destin-react/refs/heads/main/images/hexenstag.jpeg",
             },
             {
-                id: "calendrier2",
-                description: async (_perso: Perso): Promise<string> => {
-                    return "Aujourd'hui est le jour de la bénédiction de l'an nouveau. " +
-                            "Chacun est heureux d'avoir survécu à la nuit des sorcières et émerge de son foyer pour demander à la déesse Verena de bénir l'année à venir. " +
-                            "Des extraits des paroles de Verena sont lus et tous ceux qui ont de la rancoeur envers un voisin sont priés de l'exposer et de tenter de résoudre le problème devant la prétresse de Véréna.."
-                    },
-                conditions: (perso:Perso):boolean => perso.mois === enumMois.brumaire && perso.jourDuMois === 1,
-            },
-            {
                 id: "calendrier3",
                 description: async (_perso: Perso): Promise<string> => {
-                    return "Aujourd'hui est le premier jour de l'été ! Et surtout l'anniversaire de l'accession à la divinité de notre divin Sigmar ! " +
-                    "L'occasion de faire une grande fête, de danser, et de se régaler des légendaires saucisses de Sigmar en écoutant des récits des exploits de Sigmar par les prêtres et les saltimbanques. "},
+                    return "Aujourd'hui est le premier jour de l'été ! "},
                 conditions: (perso:Perso):boolean => perso.mois === enumMois.pluviose && perso.jourDuMois === 18,
             },
             {
@@ -39,27 +27,8 @@ export const evts_calendrier: GroupeEvts = {
             },
             {
                 id: "calendrier5",
-                description: async (_perso: Perso): Promise<string> => "Aujourd'hui c'est sonnstill, le jour du solsctice d'été, le plus long jour de l'année ! " +
-                    "C'est un jour de joie et de fertilité. Les jeunes couples se mettent des fleurs dans les cheveux, dansent et chantent pour honorer Taal et Rhya. " +
-                    "Et il paraît que même les elfes honorent leurs étranges dieux de la nature. ",
+                description: async (_perso: Perso): Promise<string> => "Aujourd'hui c'est le jour du solsctice d'été, le plus long jour de l'année ! ",
                 conditions: (perso:Perso):boolean => perso.mois === enumMois.floreal,
-            },
-            {
-                id: "calendrier6",
-                description: async (_perso: Perso): Promise<string> => "Aujourd'hui c'est geheiminstag, le jour des mystères. " +
-                    "Mannslieb et Morrslieb sont simultanément pleines et la frontière avec le monde des morts s'affaiblit. " +
-                    "Certaines entendent les esprit, ou vont même consulter les augures du dieu Morr. " +
-                    "Vous préférez rendre sagement hommage à vos ancêtres en demeurant chez vous. ",
-                conditions: (perso:Perso):boolean => perso.mois === enumMois.messidor,
-            },
-            {
-                id: "calendrier7",
-
-                description: async (_perso: Perso): Promise<string> => "Nous sommes en pleine semaine de la tourte hobbit. " +
-                    "Une bonne occasion de festoyer en mangeant les bonnes tourtes de ces petits vauriens boulimiques de hobbits. " +
-                    "Vivement que cela deviennent un jour férié ! " +
-                    "Cette année il va même y avoir un concours du plus gros mangeur et un de la meilleure recette. ",
-                conditions: (perso:Perso):boolean => perso.mois === enumMois.fructidor && perso.jourDuMois >= 1 && perso.jourDuMois <= 8,
             },
     ],
     probaParDefaut: 10,
