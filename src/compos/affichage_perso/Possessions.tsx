cdd983d1451e6902d1b75d5f224da645c2709ff7
gabriellevy <mdeharbe@hotmail.com>
2025-09-24 23:06:20 +0200

    afficher drogues

--- src/compos/affichage_perso/Possessions.tsx
+++ src/compos/affichage_perso/Possessions.tsx
@@ -5,6 +5,7 @@ import {PersoContexte, PersoContexteType} from "../../contexte/ContexteTypes";
 export default function Possessions() {
     const { perso } = useContext(PersoContexte) as PersoContexteType;
     return (
+        <>
         <List dense>
             <ListItemText primary={perso.statut.metalStatut} secondary={perso.statut.rang.toString()}/>
             {
@@ -13,5 +14,14 @@ export default function Possessions() {
                 ))
             }
         </List>
+            {perso.drogues.length > 0 && (<div>Drogues :</div>)}
+        <List dense>
+            {
+                perso.drogues.map((drogue: string) => (
+                    <ListItemText primary={(drogue)}/>
+                ))
+            }
+        </List>
+        </>
     );
 }
