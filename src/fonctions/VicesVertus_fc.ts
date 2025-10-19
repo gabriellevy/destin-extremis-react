d610dd335bca23087ab0f07f1dea54b130af563a
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2025-10-19 13:22:40 +0200

    correction affichage des vices

--- src/fonctions/VicesVertus_fc.ts
+++ src/fonctions/VicesVertus_fc.ts
@@ -4,6 +4,24 @@ import {Perso} from "../types/perso/Perso";
 export function descriptionViceVertus(perso: Perso, vice:Vice): string {
     const valeurVice:number = getValeurVice(perso, vice);
     switch (vice) {
+        case Vice.cruel: {
+            switch (valeurVice) {
+                case -3:
+                    return "Extrêmement " + getVertuOppose(vice);
+                case -2:
+                    return "Très " + getVertuOppose(vice);
+                case -1:
+                    return getVertuOppose(vice);
+                case 1:
+                    return vice;
+                case 2:
+                    return "Sadique";
+                case 3:
+                    return "Psychopathe";
+                default:
+                    return "valeur impossible : " + valeurVice;
+            }
+        } break;
         default : {
             switch (valeurVice) {
                 case -3:
@@ -13,11 +31,11 @@ export function descriptionViceVertus(perso: Perso, vice:Vice): string {
                 case -1:
                     return getVertuOppose(vice);
                 case 1:
-                    return "Extrêmement " + vice;
+                    return vice;
                 case 2:
                     return "Très " + vice;
                 case 3:
-                    return vice;
+                    return "Extrêmement " + vice;
                 default:
                     return "valeur impossible : " + valeurVice;
             }
